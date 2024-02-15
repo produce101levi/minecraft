ebfbc5bdb2c347a83a481961d50886bfbcd24297
Levi <Daz>
2024-02-15 10:04:34 -0600

    consola de js

---
+++ intro_js.js
@@ -0,0 +1,8 @@
+//consola (log, info, warn, error, assert)
+
+console.log("Hola");
+console.info("Esto es información");
+console.warn("Esto es una advertencia");
+console.error("Esto es un error");
+console.assert(1==1);
+console.assert(1==2);
