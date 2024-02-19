fc4b3f88e4bf42e4e20d50a30bb461691e49a3ac
Levi <Daz>
2024-02-19 09:46:09 -0600

    cambiar imagen con el DOM

---
+++ poe.js
@@ -0,0 +1,9 @@
+const boton = document.getElementById("boton_construir");
+
+boton.onclick = () => {
+    const imagen = document.getElementById("imagen_construir");
+    imagen.src = "https://phantom-marca.unidadeditorial.es/729575013c4d1737310eb16c623ee30d/resize/828/f/jpg/assets/multimedia/imagenes/2019/10/09/15706341435403.jpg";
+    console.log(imagen);
+
+};
+
