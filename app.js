1140a80d950db90ee2053787c915c6328445d8e9
Levi <Daz>
2024-02-22 10:07:42 -0600

    asynchronous sort

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const filesystem = require('fs');
 //Escribe el string del segundo parámetro
 //en el archivo indicado en el primer parámetro
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
+
+
+const arreglo = [10,500,60,30,6000];
+
+for (let item of arreglo){
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
