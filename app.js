159bf0b0392ddba0fa84fbbf11309c54eecfae16
Levi <Daz>
2024-02-22 09:49:59 -0600

    escribir un archivo desde node

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 console.log("Hola mundo desde node");
 
+//fs es el módulo para manipular el sistema de archivos
 const filesystem = require('fs');
+
+//Escribe el string del segundo parámetro
+//en el archivo indicado en el primer parámetro
+filesystem.writeFileSync('hola.txt', 'Hola desde node');
