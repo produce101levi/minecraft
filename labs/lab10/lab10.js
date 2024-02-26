aee5866bf35279775636d3587ce48c9af008d949
Levi <Daz>
2024-02-26 10:12:04 -0600

    lab10 javascript creado

---
+++ labs/lab10/lab10.js
@@ -0,0 +1,35 @@
+const http = require ('http');
+
+const server = http.createServer( (request, response) => {
+    console.log(request.url);
+
+    if (request.url == "/"){
+        response.setHeader('Content-Type', 'text/html');
+        response.write(`<!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Laboratorio 4</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+                <link rel="stylesheet" type="text/css" href="lab6.css">
+            </head>
+            <body>
+                <section class="section">
+                    <div class="container">
+                        <h1 class="title">
+                            Laboratorio 4
+                        </h1>
+                    </div>
+                </section>
+        
+                <div id='hiddenSection' class='container'></div>
+                
+                <script src="lab4.js"></script>
+            </body>
+        </html>`);
+        response.end();
+    }
+});
+
+server.listen(5000);
