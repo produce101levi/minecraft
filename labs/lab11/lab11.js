6aaa36bd704be8ad159b90266f0e696d7cd118ec
Levi <Daz>
2024-03-04 10:59:25 -0600

    agregar favicon.ico

--- labs/lab11/lab11.js
+++ labs/lab11/lab11.js
@@ -3,6 +3,8 @@ const app = express();
 
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
