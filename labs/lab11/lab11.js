28032a147adee95f72e9d86b756fedb499cbcf8d
Levi <Daz>
2024-03-04 10:53:50 -0600

    enviar archivo html como 404

--- labs/lab11/lab11.js
+++ labs/lab11/lab11.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();       
 
+const path = require('path');
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -153,11 +155,13 @@ https://github.com/produce101levi/minecraft
 </body>
 
 </html>`);
+
 });
 
 app.use((request, response, next) => {
-  console.log(request.body);
-  response.send('¡Hola mundo!'); //Manda la respuesta
+  response.sendFile(
+    path.join(__dirname, 'views', '404.html')
+  );
 });
 
 app.listen(3000);
