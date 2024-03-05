94be7217c85d7b0967e28d8fda9bdf15bd61825b
Levi <Daz>
2024-03-05 07:29:00 -0600

    configurar ejs

--- labs/lab11/lab11.js
+++ labs/lab11/lab11.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();       
 
+app.set('view engine','ejs');
+app.set('views','views');
+
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
