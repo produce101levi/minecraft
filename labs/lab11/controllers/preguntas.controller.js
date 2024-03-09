c5242ad1da8e70c3e25997a8be912cfd5464f292
Levi <Daz>
2024-03-09 08:41:28 -0600

    detalles

--- labs/lab11/controllers/preguntas.controller.js
+++ labs/lab11/controllers/preguntas.controller.js
@@ -4,19 +4,13 @@ exports.get_preguntas = (request, response, next) => {
     response.render('preguntas'); 
 };
 
-// exports.post_preguntas = (request, response, next) => {
-//     fs.writeFile('file.txt', request.body.question);
-//     preguntas.push(request.body.question);
-//     response.redirect('/');
-// };
-
-exports.submitQuestion = (req, res, next) => {
-    const question = req.body.question;
+exports.submitQuestion = (request, response, next) => {
+    const question = request.body.question;
     fs.appendFile('questions.txt', `${question}\n`, (err) => {
       if (err) throw err;
       console.log('Pregunta enviada con éxito');
     });
-    res.redirect('/preguntas');
+    response.redirect('/preguntas');
   };
 
 exports.get_root = (request, response, next) => {
